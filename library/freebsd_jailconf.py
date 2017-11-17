71030e864398c196e435d8d242670fcda496f27e
Sean Kelly <code@katron.org>
2017-11-16 21:25:49 -0500

    Use a tuple of dicts for the parameters
    Use a tuple of the dicts so the parameters stay ordered. This way the
    documentation can be generated from them as well and be stable in its
    ordering.

--- library/freebsd_jailconf.py
+++ library/freebsd_jailconf.py
@@ -34,79 +34,283 @@ RETURN = '''
 '''
 
 
-# These are the core parameters that are passed to the kernel.
+# These are the core parameters that are passed to the kernel. Descriptions for
+# the first one or two sentences from the jail(8) man page.
 JAIL_CORE_PARAMETERS = (
-    'jid',
-    'name',
-    'path',
-    'ip4.addr',
-    'ip4.saddrsel',
-    'ip4',
-    'ip6.addr',
-    'ip6.saddrsel',
-    'ip6',
-    'vnet',
-    'host.hostname',
-    'host',
-    'securelevel',
-    'devfs_ruleset',
-    'children.max',
-    'children.cur',
-    'enforce_statfs',
-    'persist',
-    'osrelease',
-    'osreldate',
-    'allow.set_hostname',
-    'allow.sysvipc',
-    'allow.raw_sockets',
-    'allow.chflags',
-    'allow.mount',
-    'allow.mount.devfs',
-    'allow.mount.fdescfs',
-    'allow.mount.nullfs',
-    'allow.mount.procfs',
-    'allow.mount.linprocfs',
-    'allow.mount.linsysfs',
-    'allow.mount.tmpfs',
-    'allow.mount.zfs',
-    'allow.quotas',
-    'allow.socket_af',
-    'linux',
-    'linux.osname',
-    'linux.osrelease',
-    'linux.oss_version',
-    'sysvmsg',
-    'sysvsem',
-    'sysvshm',
+    {
+        'name': 'name',
+        'required': True,
+        'description': ('The jail name. This is an arbitrary string that '
+                        'identifies a jail (except it may not contain a ".").'),
+        'type': 'str',
+    },
+    {
+        'name': 'path',
+        'description': 'The directory which is to be the root of the jail.',
+    },
+    {
+        'name': 'ip4.addr',
+        'description': 'A list of IPv4 addresses assigned to the jail.',
+    },
+    {
+        'name': 'ip4.saddrsel',
+        'description': ('A boolean option to disable IPv4 source address '
+                        'selection for the jail in favour of the primary IPv4 '
+                        'address of the jail.'),
+        'type': 'bool'
+    },
+    {
+        'name': 'ip4',
+        'description': 'Control the availability of IPv4 addresses.',
+    },
+    {
+        'name': 'ip6.addr',
+        'description': '',
+    },
+    {
+        'name': 'ip6.saddrsel',
+        'description': '',
+    },
+    {
+        'name': 'ip6',
+        'description': '',
+    },
+    {
+        'name': 'vnet',
+        'description': '',
+    },
+    {
+        'name': 'host.hostname',
+        'description': '',
+    },
+    {
+        'name': 'host',
+        'description': '',
+    },
+    {
+        'name': 'securelevel',
+        'description': '',
+    },
+    {
+        'name': 'devfs_ruleset',
+        'description': '',
+    },
+    {
+        'name': 'children.max',
+        'description': '',
+    },
+    {
+        'name': 'children.cur',
+        'description': '',
+    },
+    {
+        'name': 'enforce_statfs',
+        'description': '',
+    },
+    {
+        'name': 'persist',
+        'description': '',
+    },
+    {
+        'name': 'osrelease',
+        'description': '',
+    },
+    {
+        'name': 'osreldate',
+        'description': '',
+    },
+    {
+        'name': 'allow.set_hostname',
+        'description': '',
+    },
+    {
+        'name': 'allow.sysvipc',
+        'description': '',
+    },
+    {
+        'name': 'allow.raw_sockets',
+        'description': '',
+    },
+    {
+        'name': 'allow.chflags',
+        'description': '',
+    },
+    {
+        'name': 'allow.mount',
+        'description': '',
+    },
+    {
+        'name': 'allow.mount.devfs',
+        'description': '',
+    },
+    {
+        'name': 'allow.mount.fdescfs',
+        'description': '',
+    },
+    {
+        'name': 'allow.mount.nullfs',
+        'description': '',
+    },
+    {
+        'name': 'allow.mount.procfs',
+        'description': '',
+    },
+    {
+        'name': 'allow.mount.linprocfs',
+        'description': '',
+    },
+    {
+        'name': 'allow.mount.linsysfs',
+        'description': '',
+    },
+    {
+        'name': 'allow.mount.tmpfs',
+        'description': '',
+    },
+    {
+        'name': 'allow.mount.zfs',
+        'description': '',
+    },
+    {
+        'name': 'allow.quotas',
+        'description': '',
+    },
+    {
+        'name': 'allow.socket_af',
+        'description': '',
+    },
+    {
+        'name': 'linux',
+        'description': '',
+    },
+    {
+        'name': 'linux.osname',
+        'description': '',
+    },
+    {
+        'name': 'linux.osrelease',
+        'description': '',
+    },
+    {
+        'name': 'linux.oss_version',
+        'description': '',
+    },
+    {
+        'name': 'sysvmsg',
+        'description': '',
+    },
+    {
+        'name': 'sysvsem',
+        'description': '',
+    },
+    {
+        'name': 'sysvshm',
+        'description': '',
+    },
 )
 
 # These parameters are only used by jail(8).
 JAIL_PSUEDO_PARAMETERS = (
-    'exec.prestart',
-    'exec.start',
-    'command',
-    'exec.poststart',
-    'exec.prestop',
-    'exec.stop',
-    'exec.poststop',
-    'exec.clean',
-    'exec.jail_user',
-    'exec.system_jail_user',
-    'exec.system_user',
-    'exec.timeout',
-    'exec.consolelog',
-    'exec.fib',
-    'stop.timeout',
-    'interface',
-    'vnet.interface',
-    'ip_hostname',
-    'mount',
-    'mount.fstab',
-    'mount.devfs',
-    'mount.fdescfs',
-    'mount.procfs',
-    'allow.dying',
-    'depend',
+    {
+        'name': 'exec.prestart',
+        'description': '',
+    },
+    {
+        'name': 'exec.start',
+        'description': '',
+    },
+    {
+        'name': 'command',
+        'description': '',
+    },
+    {
+        'name': 'exec.poststart',
+        'description': '',
+    },
+    {
+        'name': 'exec.prestop',
+        'description': '',
+    },
+    {
+        'name': 'exec.stop',
+        'description': '',
+    },
+    {
+        'name': 'exec.poststop',
+        'description': '',
+    },
+    {
+        'name': 'exec.clean',
+        'description': '',
+    },
+    {
+        'name': 'exec.jail_user',
+        'description': '',
+    },
+    {
+        'name': 'exec.system_jail_user',
+        'description': '',
+    },
+    {
+        'name': 'exec.system_user',
+        'description': '',
+    },
+    {
+        'name': 'exec.timeout',
+        'description': '',
+    },
+    {
+        'name': 'exec.consolelog',
+        'description': '',
+    },
+    {
+        'name': 'exec.fib',
+        'description': '',
+    },
+    {
+        'name': 'stop.timeout',
+        'description': '',
+    },
+    {
+        'name': 'interface',
+        'description': '',
+    },
+    {
+        'name': 'vnet.interface',
+        'description': '',
+    },
+    {
+        'name': 'ip_hostname',
+        'description': '',
+    },
+    {
+        'name': 'mount',
+        'description': '',
+    },
+    {
+        'name': 'mount.fstab',
+        'description': '',
+    },
+    {
+        'name': 'mount.devfs',
+        'description': '',
+    },
+    {
+        'name': 'mount.fdescfs',
+        'description': '',
+    },
+    {
+        'name': 'mount.procfs',
+        'description': '',
+    },
+    {
+        'name': 'allow.dying',
+        'description': '',
+    },
+    {
+        'name': 'depend',
+        'description': '',
+    },
 )
 
 import textwrap
@@ -160,10 +364,6 @@ class FreeBsdJail(object):
 def main():
 
     module_args = {
-        'name': {
-            'required': True,
-            'type': 'str',
-        },
         'conf_file': {
             'default': '/etc/jail.conf',
             'required': False,
